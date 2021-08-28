5f513281c03c29f32b109cf4ba359f2dd7ec6498
Brandon Mellus <mellusbrandon@gmail.com>
2021-08-28 09:56:03 -0400

    Setup rendering of home page

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,37 @@
 const express = require('express');
 
+const { User, Post } = require('../models');
+
 const apiRouter = require('./api');
 
 const router = express.Router();
 
 router.use('/api', apiRouter);
 
-router.get('/', (req, res) => {
-    res.render('home');
-})
+router.get('/', async (req, res) => {
+    try {
+        const posts = (await Post.findAll({
+            include: [User]
+        })).map(postData => {
+            const post = postData.get();
+            const user = post.user.get();
+            return {
+                ...post,
+                user
+            }
+        });
+    
+        res.render(
+            'home',
+            {
+                heading: 'Tech Blog',
+                posts,
+                isLoggedin: req.session.logged_in
+            }
+        );
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+});
 
 module.exports = router;
