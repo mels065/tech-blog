75603e2e8d7c5a3beff7bc40e365a824c9b35358
Brandon Mellus <mellusbrandon@gmail.com>
2021-08-28 22:32:33 -0400

    Render individual posts

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const apiRouter = require('./api');
 
@@ -30,7 +30,55 @@ router.get('/', async (req, res) => {
             }
         );
     } catch (err) {
-        res.status(500).json(err.message);
+        res.status(400).end()
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const postData = await Post.findByPk(
+            id,
+            { 
+                include: [
+                    User,
+                    {
+                        model: Comment,
+                        include: [User]
+                    }
+                ]
+            }
+        );
+
+        if (postData) {
+            const post = postData.get();
+            post.user = post.user.get();
+            post.comments = post.comments.map(commentData => {
+                const comment = commentData.get()
+                comment.user = comment.user.get();
+                return comment;
+            });
+            
+            res.render(
+                'post',
+                {
+                    heading: 'Tech Blog',
+                    post,
+                    isLoggedIn: req.session.is_loggedin
+                }
+            )
+        } else {
+            res.render(
+                '404',
+                {
+                    heading: 'Tech Blog',
+                    isLoggedIn: req.session.is_loggedin
+                }
+            )
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(400).end();
     }
 });
 
