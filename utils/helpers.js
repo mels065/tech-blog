d129674c9c6012d4127948b7147fe9e4f54ac1fc
Brandon Mellus <mellusbrandon@gmail.com>
2021-09-08 16:10:22 -0700

    Render post and comment content so that new paragraphs will be enclosed in <p> tags

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,5 +5,9 @@ module.exports = {
 
     is_current_user_comment(currentUserId, commentUserId) {
         return currentUserId === commentUserId;
+    },
+
+    render_blog_content(content) {
+        return content.split('\n').map(paragraph => `<p>${paragraph}</p>`).join('\n');
     }
 };
